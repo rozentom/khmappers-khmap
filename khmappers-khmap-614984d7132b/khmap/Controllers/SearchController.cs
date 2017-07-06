dc14195b120464fa5257e7ad1c1b3250b23eed08
rozentom <rozentom@post.bgu.ac.il>
2017-07-06 13:35:04 +0300

    stash

--- khmappers-khmap-614984d7132b/khmap/Controllers/SearchController.cs
+++ khmappers-khmap-614984d7132b/khmap/Controllers/SearchController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using MongoDB.Bson;
 using khmap.Models;
+using WebApplication2.Search;
 
 namespace khmap.Controllers
 {
@@ -20,12 +21,14 @@ namespace khmap.Controllers
         private UserDB _userService;
         private GroupDB _groupManager;
         private ApplicationUserManager _userManager;
+        private Search _searchObj;
 
         public SearchController()
         {
             this._mapManager = new MapDB(new Settings());
             this._userService = new UserDB(new Settings());
             this._groupManager = new GroupDB(new Settings());
+            _searchObj = new Search();
             //UserManager = UserManager;
         }
 
@@ -48,6 +51,9 @@ namespace khmap.Controllers
             svm.Maps = _mapManager.SearchMap(text);
             svm.Groups = _groupManager.SearchGroup(text);
             svm.Users = _userService.SearchUser(text);
+            svm.Maps = _searchObj.searchMaps(text);
+            svm.Groups = _searchObj.searchGroups(text);
+            svm.Users = _searchObj.searchUsers(text);
             svm.SearchText = text;
             return View(svm);
         }
