5a22e16fef7827b2382a564d5c15979f6d866456
rozentom <rozentom@post.bgu.ac.il>
2017-07-11 15:47:34 +0300

    added: -shlomi's work -showing folders and their maps
    fixed:
    -1000000 bugs found in shlomi's staff

---
+++ khmappers-khmap-614984d7132b/khmap/khmap/Controllers/MapFolderController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Bson;
+using Microsoft.AspNet.Identity;
+using khmap.DataBaseProviders;
+using khmap.Models;
+
+namespace khmap.Controllers
+{
+    public class MapFolderController : Controller
+    {
+        private MapFolderDB _mapFolderDataManager;
+        private ApplicationUserManager _userManager;
+        private MapDB _mapDataManager;
+        private GroupDB _groupManager;
+        public static MapFolder STATIC_FOLDER;
+        public static MapFolder STATIC_INNER_FOLDER;
+
+
+        public MapFolderController()
+        {
+            this._mapFolderDataManager = new MapFolderDB(new Settings());
+            this._groupManager = new GroupDB(new Settings());
+        }
+
+        // GET: MapFolder
+        public ActionResult Index(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                var Folderid = new ObjectId(id);
+                var Folder = _mapFolderDataManager.GetMapFolderById(Folderid);
+                if (Folder != null)
+                {
+                    var userId = User.Identity.GetUserId();
+                    bool isFollowing = Folder.Followers.Contains(new ObjectId(userId), new IDComparer());
+                    ViewBag.isFollowing = isFollowing;
+                    MapPermissionController mpc = new MapPermissionController();
+                   // MapViewModel mvm = new MapViewModel { Id = map.Id, Name = map.Name, CreationTime = map.CreationTime, CreatorId = map.Creator, CreatorEmail = UserManager.GetEmail(map.Creator.ToString()), Description = map.Description, Model = map.Model, ModelArchive = map.MapsArchive };
+                    if (mpc.IsMapOwner(id, User.Identity.GetUserId()))
+                    {
+                        return View();
+                    }
+                    else
+                    {
+                        return View();
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        /** public ActionResult createFolder(MapFolder parent)
+         {
+             var id = User.Identity.GetUserId();
+             ObjectId UserId = new ObjectId(id);
+             MapFolder toadd = new MapFolder();
+             toadd.Creator = UserId;
+         }**/
+
+       private MapFolder createSuppFolderLocaly()
+        {
+            _mapFolderDataManager = new MapFolderDB(new Settings());
+
+            var id = User.Identity.GetUserId();
+            ObjectId UserId = new ObjectId(id);
+
+            MapPermissions mapPermissions = new MapPermissions();
+            mapPermissions.Owner = new KeyValuePair<ObjectId, MapPermissionType>(UserId, MapPermissionType.RW);
+            mapPermissions.Users = new Dictionary<ObjectId, MapPermissionType>();
+            mapPermissions.Groups = new Dictionary<ObjectId, MapPermissionType>();
+            mapPermissions.AllUsers = MapPermissionType.NA;
+
+            mapPermissions.Users.Add(UserId, MapPermissionType.RW);
+
+            MapFolder suppFolder = new MapFolder();
+            suppFolder.Name = "suppFolder";
+            suppFolder.Creator = UserId;
+            suppFolder.CreationTime = DateTime.Now;
+            suppFolder.Description = "Supirior Folder";
+            suppFolder.Followers = new HashSet<ObjectId>();
+            suppFolder.Permissions = mapPermissions;
+            suppFolder.idOfMapsInFolder = new HashSet<ObjectId>();
+            suppFolder.idOfSubFolders = new HashSet<ObjectId>();
+            suppFolder.ParentDierctory = new ObjectId();
+            suppFolder.FirstFolderOfUser = UserId;
+            var maps = new MapDB(new Settings()).GetAllMaps();
+            foreach (Map map in maps)
+            {
+                suppFolder.idOfMapsInFolder.Add(map.Id);
+            }
+            return suppFolder;
+
+        }
+
+        public ActionResult MyFirstMapAndFolders()
+        {
+            try
+            {
+                var id = User.Identity.GetUserId();
+                ObjectId UserId = new ObjectId(id);
+                MapFolder superiorMapFolder = _mapFolderDataManager.GetSuperiorMapFolderOfUser(UserId);
+                if(superiorMapFolder == null)
+                {
+                    superiorMapFolder = createSuppFolderLocaly();
+                    _mapFolderDataManager.AddFolder(superiorMapFolder);
+                }
+                var mapFolders = this._mapFolderDataManager.GetAllSubFolder(superiorMapFolder);
+         
+                //var mapFolders = this._mapFolderDataManager.GetFirstFoldersOfUser(UserId);
+                var maps = this._mapFolderDataManager.GetAllMapsInFolder(superiorMapFolder);
+                _mapDataManager = new MapDB(new Settings());
+                //var maps = _mapDataManager.GetAllMaps();
+                //MapFolder tempSingleFolder = new MapFolder();
+                //tempSingleFolder.Name = "try plz work";
+                //ObjectId mapID = maps.First().Id;
+                //tempSingleFolder.idOfMapsInFolder = new HashSet<ObjectId>();
+                //tempSingleFolder.idOfMapsInFolder.Add(mapID);
+                //MapFolder innerFolder = new MapFolder();
+                //innerFolder.Name = "inner";
+                //innerFolder.idOfMapsInFolder = new HashSet<ObjectId>();
+                //innerFolder.idOfMapsInFolder.Add(mapID);
+                //string innerFolderId = maps.First().Id.ToString().Substring(1);
+                //innerFolder.Id = new ObjectId("6" + innerFolderId);
+                //tempSingleFolder.idOfSubFolders = new HashSet<ObjectId>();
+                //tempSingleFolder.idOfSubFolders.Add(innerFolder.Id);
+                //tempSingleFolder.Id = new ObjectId("7" + innerFolderId);
+                //List<MapFolder> tempFolders = new List<MapFolder>() { tempSingleFolder };
+                //ViewBag.maps = new List<Map>();
+                //STATIC_FOLDER = tempSingleFolder;
+                //STATIC_INNER_FOLDER = innerFolder;
+                ViewBag.maps = maps;
+                ViewBag.currFolder = superiorMapFolder;
+                return PartialView("_MyFoldersView", mapFolders);
+            }
+            catch (Exception e)
+            {
+                string exp = e.ToString();
+                //return RedirectToAction("Index", "Home");
+                _mapDataManager = new MapDB(new Settings());
+                ViewBag.maps = _mapDataManager.GetAllMaps();
+                return PartialView("_MyFoldersView", new List<MapFolder>());
+
+            }
+        }
+
+        public ActionResult OpenFolder(string Id)
+        {
+
+            //var id = User.Identity.GetUserId();
+            // ObjectId FolderId = new ObjectId(toOpenId);
+            //var FolderId = new ObjectId(Id);
+            //var parent = this._mapFolderDataManager.GetMapFolderById(FolderId);
+            var parent = STATIC_FOLDER;
+            //MapFolder superiorMapFolder = _mapFolderDataManager.GetSuperiorMapFolderOfUser(UserId);
+            //var mapFolders = this._mapFolderDataManager.GetAllSubFolder(superiorMapFolder);
+            var mapFolders = this._mapFolderDataManager.GetAllSubFolder(parent);
+            mapFolders = new List<MapFolder>() { STATIC_INNER_FOLDER };
+            var maps = this._mapFolderDataManager.GetAllMapsInFolder(parent);
+            ViewBag.maps = maps;
+            //return "y u no work";
+            return PartialView("_MyFoldersView", mapFolders);
+
+        }
+
+
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+                var map = this._mapFolderDataManager.GetMapFolderById(new ObjectId(id));
+                if (IsValidId(id) && IsValidMap(map) )
+                {
+                    var mdvm = new MapDeleteViewModel { Id = map.Id.ToString(), Name = map.Name, CreatorEmail = _userManager.GetEmail(map.Creator.ToString()), CreationTime = map.CreationTime, Description = map.Description };
+                    return View(mdvm);
+                }
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool IsValidId(string id)
+        {
+            if (string.IsNullOrEmpty(id) || string.IsNullOrWhiteSpace(id))
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        private bool IsValidMap(MapFolder folder)
+        {
+            return folder != null;
+        }
+    }
+}
---
+++ khmappers-khmap-614984d7132b/khmap/khmap/Models/Map.cs
@@ -0,0 +1,84 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace khmap.Models
+{
+
+
+    public enum MapPermissionType
+    {
+        [Display(Name = "ReadWrite")]
+        RW,      // Can Edit map
+
+        [Display(Name = "ReadOnly")]
+        RO,    // Can view map only
+
+        [Display(Name = "NoAccess")]
+        NA // No Access
+    };
+
+    public class Map
+    {
+        [BsonId]
+        public ObjectId Id { get;
+            set; }
+        public string Name { get; set; }
+        public ObjectId Creator { get; set; }
+        public DateTime CreationTime { get; set; }
+        public string Description { get; set; }
+        public BsonDocument Model { get; set; }
+        public MapPermissions Permissions { get; set; }
+        public HashSet<ObjectId> Followers { get; set; }
+        public Queue<MapVersion> MapsArchive { get; set; }
+    }
+
+    public class MapVersion
+    {
+        public static readonly int VERSIONS = 5;
+        public DateTime CreationTime { get; set; }
+        public BsonDocument Model { get; set; }
+    }
+
+    public class MapPermissions
+    {
+        public KeyValuePair<ObjectId, MapPermissionType> Owner { get; set; }
+        public Dictionary<ObjectId, MapPermissionType> Users { get; set; }
+        public Dictionary<ObjectId, MapPermissionType> Groups { get; set; }
+        public MapPermissionType AllUsers { get; set; }
+    }
+
+
+    class MapComparer : IEqualityComparer<Map>
+    {
+
+        public bool Equals(Map x, Map y)
+        {
+            return x.Id.ToString().Equals(y.Id.ToString());
+        }
+
+        public int GetHashCode(Map obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+
+    class IDComparer : IEqualityComparer<ObjectId>
+    {
+
+        public bool Equals(ObjectId x, ObjectId y)
+        {
+            return x.ToString().Equals(y.ToString());
+        }
+
+        public int GetHashCode(ObjectId obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+}
